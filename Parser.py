cbf9d48971f01f0905ce1b614a83f296d64c29a4
Moshii-Xz <79061457+Moshii-Xz@users.noreply.github.com>
2024-05-17 14:16:08 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,47 +1,53 @@
-import os
 import xml.etree.ElementTree as ET
 
-# Obtener la ruta completa del archivo XML
-script_dir = os.path.dirname(__file__)  # Objeto de la clase os.PathLike que representa el directorio del script
-xml_file = os.path.join(script_dir, "schema.xml")  # Método join() de la clase os.PathLike, que devuelve una ruta completa
+# Clase base para todos los elementos XML
+class XMLElement:
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
 
-# Leer el contenido del archivo XML
-with open(xml_file, "r") as file:
-    xml_data = file.read()  # Método read() de la clase TextIOWrapper que devuelve el contenido del archivo como una cadena de texto
-    print("Contenido del archivo XML:")
-    print(xml_data)
-    print("\n")
+    def display(self, indent=0):
+        attrs = ", ".join(f"{k}: {v}" for k, v in self.attributes.items())
+        print(' ' * indent + f"Element: {self.name}, Attributes: {{{attrs}}}")
 
-# Parse XML
-root = ET.fromstring(xml_data)  # Método fromstring() de la clase ElementTree, que devuelve el elemento raíz del árbol XML
+# Clase para elementos simples
+class SimpleElement(XMLElement):
+    def __init__(self, name, attributes):
+        super().__init__(name, attributes)
 
-# Procesar el esquema
-# Iterar sobre los elementos 'element' del esquema XML
-for element in root.findall('.//{http://www.w3.org/2001/XMLSchema}element'):  
-    name = element.get('name')  # Método get() de la clase Element, que devuelve el valor del atributo 'name'
-    type_ = element.get('type')  # Método get() de la clase Element, que devuelve el valor del atributo 'type'
-    print(f"Elemento: {name}, Tipo: {type_}")
+# Clase para elementos complejos que contienen otros elementos
+class ComplexElement(XMLElement):
+    def __init__(self, name, attributes):
+        super().__init__(name, attributes)
+        self.children = []
 
-# Iterar sobre los elementos 'complexType' del esquema XML
-for complex_type in root.findall('.//{http://www.w3.org/2001/XMLSchema}complexType'):  
-    name = complex_type.get('name')  # Método get() de la clase Element, que devuelve el valor del atributo 'name'
-    print(f"Tipo Complejo: {name}")
-    # Iterar sobre los elementos 'element' dentro de 'complexType'
-    for child in complex_type.findall('.//{http://www.w3.org/2001/XMLSchema}element'):
-        child_name = child.get('name')  # Método get() de la clase Element, que devuelve el valor del atributo 'name'
-        child_type = child.get('type')  # Método get() de la clase Element, que devuelve el valor del atributo 'type'
-        print(f"    Elemento: {child_name}, Tipo: {child_type}")
+    def add_child(self, child):
+        self.children.append(child)
 
-    # Iterar sobre los elementos 'sequence' dentro de 'complexType'
-    for sequence in complex_type.findall('.//{http://www.w3.org/2001/XMLSchema}sequence'):  
-        # Iterar sobre los elementos 'element' dentro de 'sequence'
-        for child in sequence.findall('.//{http://www.w3.org/2001/XMLSchema}element'):  
-            child_name = child.get('name')  # Método get() de la clase Element, que devuelve el valor del atributo 'name'
-            child_type = child.find('{http://www.w3.org/2001/XMLSchema}complexType/{http://www.w3.org/2001/XMLSchema}sequence')  # Método find() de la clase Element, que busca elementos secundarios que coincidan con la ruta dada
-            # Verificar si child_type no es None
-            if child_type is not None:
-                # Iterar sobre los elementos secundarios dentro de child_type
-                for inner_child in child_type.findall('.//{http://www.w3.org/2001/XMLSchema}element'):
-                    inner_child_name = inner_child.get('name')  # Método get() de la clase Element, que devuelve el valor del atributo 'name'
-                    inner_child_type = inner_child.get('type')  # Método get() de la clase Element, que devuelve el valor del atributo 'type'
-                    print(f"    Elemento: {child_name}/{inner_child_name}, Tipo: {inner_child_type}")
+    # Polimorfismo: Sobrescritura del método display para incluir los hijos
+    def display(self, indent=0):
+        super().display(indent)
+        for child in self.children:
+            child.display(indent + 2)
+
+# Función para parsear el XML y construir el árbol de elementos
+def parse_element(element):
+    attributes = element.attrib
+    if list(element):
+        complex_element = ComplexElement(element.tag.split('}')[-1], attributes)
+        for child in element:
+            complex_element.add_child(parse_element(child))
+        return complex_element
+    else:
+        return SimpleElement(element.tag.split('}')[-1], attributes)
+
+# Función principal para leer y procesar el archivo XML
+def process_xml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    root_element = parse_element(root)
+    root_element.display()
+
+# Ejecución del programa
+if __name__ == "__main__":
+    process_xml("schema.xml")
